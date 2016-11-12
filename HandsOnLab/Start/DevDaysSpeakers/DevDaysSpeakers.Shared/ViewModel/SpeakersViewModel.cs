3bb694868367e612714f60a760a7d4ff11e9d7dd
Stephen2Page <stephenpage@MBPro-Retina.local>
2016-11-12 14:35:39 -0500

    SpeakerView (list) working

--- HandsOnLab/Start/DevDaysSpeakers/DevDaysSpeakers.Shared/ViewModel/SpeakersViewModel.cs
+++ HandsOnLab/Start/DevDaysSpeakers/DevDaysSpeakers.Shared/ViewModel/SpeakersViewModel.cs
@@ -16,7 +16,80 @@ using System.Runtime.CompilerServices;
 
 namespace DevDaysSpeakers.ViewModel
 {
-    public class SpeakersViewModel
-    {
-    }
+
+	public class SpeakersViewModel : INotifyPropertyChanged
+	{
+		public ObservableCollection<Speaker> Speakers { get; set; }
+
+		public Command GetSpeakersCommand { get; set; }
+
+		public SpeakersViewModel()
+		{
+			Speakers = new ObservableCollection<Speaker>();
+			GetSpeakersCommand = new Command(
+				async () => await GetSpeakers(),
+				() => !IsBusy);
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private bool busy;
+
+		public bool IsBusy
+		{
+			get { return busy; }
+			set
+			{
+				busy = value;
+				OnPropertyChanged();
+				//Update the can execute
+				GetSpeakersCommand.ChangeCanExecute();
+			}
+		}
+
+		private void OnPropertyChanged([CallerMemberName] string name = null) =>
+	PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+private async Task GetSpeakers()
+		{
+			if (IsBusy)
+				return;
+
+			Exception error = null;
+			try
+			{
+				IsBusy = true;
+
+				using (var client = new HttpClient())
+				{
+					//grab json from server
+					var json = await client.GetStringAsync("http://demo4404797.mockable.io/speakers");
+
+					//Deserialize json
+					var items = JsonConvert.DeserializeObject<List<Speaker>>(json);
+
+					//Load speakers into list
+					Speakers.Clear();
+					foreach (var item in items)
+						Speakers.Add(item);
+				}
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine("Error: " + ex);
+				error = ex;
+			}
+			finally
+			{
+				IsBusy = false;
+			}
+
+			//If anything goes wrong the catch will save the exception and AFTER the finally block we can pop up an alert:
+
+			if (error != null)
+				await Application.Current.MainPage.DisplayAlert("Error!", error.Message, "OK");
+		}
+
+	}
+
 }
